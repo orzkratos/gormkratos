d98b6d68833a6f0ae7b96f32c46a27bb44235699
yangyile <example@gogit.com>
2024-08-25 17:42:56 +0700

    简单修改

--- gormkratos.go
+++ gormkratos.go
@@ -12,19 +12,19 @@ func Transaction(
 	ctx context.Context,
 	db *gorm.DB,
 	fc func(db *gorm.DB) *errors.Error,
-	erkTxFc func(err error) *errors.Error,
+	newErkTx func(error) *errors.Error,
 	opts ...*sql.TxOptions,
 ) (erk *errors.Error) {
-	if err := db.WithContext(ctx).Transaction(func(db *gorm.DB) error {
+	if etx := db.WithContext(ctx).Transaction(func(db *gorm.DB) error {
 		if erk = fc(db); erk != nil {
 			return erk
 		}
 		return nil
-	}, opts...); err != nil {
+	}, opts...); etx != nil {
 		if erk != nil {
 			return erk
 		}
-		return erkTxFc(err)
+		return newErkTx(etx)
 	}
 	return nil
 }
